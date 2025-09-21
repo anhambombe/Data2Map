e55b7d3f03ea3759e517bb5bdbc20801854f3943
anhambombe <84500027+anhambombe@users.noreply.github.com>
2025-09-21 22:06:38 +0100

    Update maping1a.py

--- maping1a.py
+++ maping1a.py
@@ -570,5 +570,10 @@ st.sidebar.subheader(":rainbow[Mapa Coroplético]")
 
 #st.sidebar.link_button("SCIDaR", "https://scidar.org")
 #st.sidebar.image("https://scidar.org/wp-content/uploads/2021/02/cropped-Big_no-bg-1-1.png", caption="", use_container_width=True)
-st.sidebar.link_button("Baixar versão desktop do App", "https://drive.google.com/uc?export=download&id=1Y--1shu7FwsCD5QAckQOXsg_MrxXqTywOSsK")
+st.sidebar.link_button(
+    "Baixar versão desktop do App", 
+    "https://drive.google.com/uc?export=download&id=1shu7FwsCD5QAckQOXsg_MrxXqTywOSsK"
+)
+
 #https://drive.google.com/file/d/1shu7FwsCD5QAckQOXsg_MrxXqTywOSsK/view?usp=sharing
+https://drive.google.com/file/d/1shu7FwsCD5QAckQOXsg_MrxXqTywOSsK/view?usp=sharing
