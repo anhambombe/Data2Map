048caf52a0e76d14a82a4dbc2c51cc8f7bc76d1b
anhambombe <84500027+anhambombe@users.noreply.github.com>
2025-09-23 23:18:08 +0100

    Update maping1a.py

--- maping1a.py
+++ maping1a.py
@@ -468,6 +468,7 @@ with tab1:
     m = folium.Map(location=[-11.2, 17.8], zoom_start=6, tiles=None, control_scale=True)
     with st.sidebar.expander("⚙ Carregar camada "):
         shapefile_prov = st.file_uploader("Carregue a camada (.zip)", type=["zip"])
+        message_placeholder.empty()
         if shapefile_prov:
             gdf_prov = load_shapefile(shapefile_prov)
             message_placeholder.info("Carregando arquivos...")
